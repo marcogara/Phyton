e9389492c5f3d9d7bb98da3c0403bac088829ed3
marcogara <103924910+marcogara@users.noreply.github.com>
2023-07-11 14:45:55 +0200

    Add files via upload

---
+++ babysteps.py
@@ -0,0 +1,130 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print("Hi there")
+
+
+#age = 20
+#price = 19.95
+
+first_name = "Marco"
+is_online = True
+
+print("Exercise")
+patient_name = 'John Smith'
+patient_age = 20
+is_new_patient = True
+
+''' 
+Exercise to calculate your birthday 
+
+name = input("What is your name? ")
+
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+
+age = 2023 - int(birth_year)
+
+print(age)
+
+
+'''
+
+'''
+Exercise to sum 2 numbers 
+
+
+first_num = float(input("Enter first number: "))
+sec_num = float(input("Enter second number: "))
+
+sum = first_num + sec_num
+
+print("Sum : "+ str(sum))
+
+'''
+
+'''
+alternative Ex. sum 
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+'''
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course)
+
+print (course.find('y'))
+
+print (course.replace('for','4'))
+
+print ('Python' in course) # check if there is the word "Python" in the variable string course
+
+'''
+Arithmetic Operator
+'''
+print (10 //  3 )
+print (10 / 3)
+
+print (10%3)
+
+print (10**3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
